ed49471edb9a724e0fa7b6ef72e11429e5a5d634
Stewart MacKenzie-Leigh <stewartml@zoho.com>
2013-03-16 14:40:52 +0000

    Changes GhostState: trying different neural network input features.

--- agent/src/pacman/entries/pacman/logging/GhostState.java
+++ agent/src/pacman/entries/pacman/logging/GhostState.java
@@ -19,7 +19,7 @@ public class GhostState
 	public int edibleTime;
 	
 	public MOVE avoidPacman;
-	public MOVE chasePacman;
+	public MOVE chasePacmanM, chasePacmanP, chasePacmanE;
 	
 	public double distanceToPacman;
 	
@@ -40,7 +40,10 @@ public class GhostState
 		MOVE lastMove = game.getGhostLastMoveMade(ghost);
 		
 		avoidPacman = game.getNextMoveAwayFromTarget(ghostIndex, pacmanIndex, lastMove, DM.MANHATTAN);
-		chasePacman = game.getNextMoveTowardsTarget(ghostIndex, pacmanIndex, lastMove, DM.MANHATTAN);
+		
+		chasePacmanM = game.getNextMoveTowardsTarget(ghostIndex, pacmanIndex, lastMove, DM.MANHATTAN);
+		chasePacmanP = game.getNextMoveTowardsTarget(ghostIndex, pacmanIndex, lastMove, DM.PATH);
+		chasePacmanE = game.getNextMoveTowardsTarget(ghostIndex, pacmanIndex, lastMove, DM.EUCLID);
 		
 		distanceToPacman = game.getDistance(ghostIndex, pacmanIndex, DM.MANHATTAN);
 	}
@@ -51,23 +54,33 @@ public class GhostState
 		//normalise to between -1 and +1
 		return new double[]
 		{
-			(double)levelTime / 4000,
-			(Math.log(edibleScore / 100) / Math.log(2)) / 5,
-			(double)activePowerPills / 4,
-			(double)pacmanLives / 4,
+			/* 1*/ (double)levelTime / 4000,
+			/* 2*/ (Math.log(edibleScore / 100) / Math.log(2)) / 5,
+			/* 3*/ (double)activePowerPills / 4,
+			/* 4*/ (double)pacmanLives / 4,
 			
-			(double)edibleTime / 200,
-			(avoidPacman == MOVE.UP ? 1 : 0),
-			(avoidPacman == MOVE.DOWN ? 1 : 0),
-			(avoidPacman == MOVE.LEFT ? 1 : 0),
-			(avoidPacman == MOVE.RIGHT ? 1 : 0),
+			/* 5*/ (double)edibleTime / 200,
+			/* 6*/ (avoidPacman == MOVE.UP ? 1 : 0),
+			/* 7*/ (avoidPacman == MOVE.DOWN ? 1 : 0),
+			/* 8*/ (avoidPacman == MOVE.LEFT ? 1 : 0),
+			/* 9*/ (avoidPacman == MOVE.RIGHT ? 1 : 0),
 			
-			(chasePacman == MOVE.UP ? 1 : 0),
-			(chasePacman == MOVE.DOWN ? 1 : 0),
-			(chasePacman == MOVE.LEFT ? 1 : 0),
-			(chasePacman == MOVE.RIGHT ? 1 : 0),
+			/*10*/ (chasePacmanM == MOVE.UP ? 1 : 0),
+			/*11*/ (chasePacmanM == MOVE.DOWN ? 1 : 0),
+			/*12*/ (chasePacmanM == MOVE.LEFT ? 1 : 0),
+			/*13*/ (chasePacmanM == MOVE.RIGHT ? 1 : 0),
 			
-			distanceToPacman / 100
+			/*14*/ (chasePacmanP == MOVE.UP ? 1 : 0),
+			/*15*/ (chasePacmanP == MOVE.DOWN ? 1 : 0),
+			/*16*/ (chasePacmanP == MOVE.LEFT ? 1 : 0),
+			/*17*/ (chasePacmanP == MOVE.RIGHT ? 1 : 0),
+			
+			/*18*/ (chasePacmanE == MOVE.UP ? 1 : 0),
+			/*19*/ (chasePacmanE == MOVE.DOWN ? 1 : 0),
+			/*20*/ (chasePacmanE == MOVE.LEFT ? 1 : 0),
+			/*21*/ (chasePacmanE == MOVE.RIGHT ? 1 : 0),
+			
+			/*22*/ distanceToPacman / 100
 		};
 	}
 	
