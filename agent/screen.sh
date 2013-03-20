302f37e0ae91b9582d43f5c493a48db504060812
Stewart MacKenzie-Leigh <me@stewartml.co.uk>
2013-03-20 21:44:32 +0000

    Improves screen.sh

--- agent/screen.sh
+++ agent/screen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for i in {13..20}
 do
-    ssh telford-$i screen -d -m "/home/student/c/cxb07142/PC/4th\ Year/Project/agent/run.sh > /home/student/c/cxb07142/PC/4th\ Year/Project/agent/runs/telford-$i.txt"
+    screen -d -m ssh telford-$i "/home/student/c/cxb07142/PC/4th\ Year/Project/agent/run.sh | tee /home/student/c/cxb07142/PC/4th\ Year/Project/agent/runs/telford-$i-1.txt"
+    screen -d -m ssh telford-$i "/home/student/c/cxb07142/PC/4th\ Year/Project/agent/run.sh | tee /home/student/c/cxb07142/PC/4th\ Year/Project/agent/runs/telford-$i-2.txt"
 done
 
