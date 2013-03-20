eac392a301aaf259f90d85cd1fba9a6c556375f3
Stewart MacKenzie-Leigh <stewartml@zoho.com>
2013-03-20 15:49:05 +0000

    Adds test scripts.

---
+++ agent/scripts/nonlearning-legacy.js
@@ -0,0 +1,12 @@
+
+/*
+Non-learning ghost model against the Legacy controller.
+*/
+
+nodeExpansionThreshold = 30;
+maximumSimulationLength = 100000;
+pacManModel = new RandomNonRevPacMan();
+ghostModel = new Legacy();
+selectionPolicy = new LevineUcbSelectionPolicy(4000);
+opponent = new Legacy();
+
