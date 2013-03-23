ec1eb002b8e387ed084dc8bd8e4dda3748656f07
Stewart MacKenzie-Leigh <me@stewartml.co.uk>
2013-03-23 11:15:26 +0000

    Updates computer numbers in screen.sh script.

--- agent/screen.sh
+++ agent/screen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {13..20}
+for i in {14..25}
 do
     screen -d -m ssh telford-$i "/home/student/c/cxb07142/PC/4th\ Year/Project/agent/run.sh 2> >(tee -a /home/student/c/cxb07142/PC/4th\ Year/Project/agent/error.log >&2) "
 done
