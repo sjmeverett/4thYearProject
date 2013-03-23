22e65ab5cfafef0b93f58ba96dc108bbd640ff91
Stewart MacKenzie-Leigh <me@stewartml.co.uk>
2013-03-23 11:48:11 +0000

    Bug fix.

--- agent/runwithlog.sh
+++ agent/runwithlog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-ssh telford-$1 ./run.sh > >(tee -a run.log) 2> >(tee -a error.log >&2)
+ssh telford-$1 /home/student/c/cxb07142/PC/4th\ Year/Project/agent/run.sh > >(tee -a run.log) 2> >(tee -a error.log >&2)
