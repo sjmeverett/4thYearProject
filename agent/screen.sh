46060bd8d96577ac6c41958cc27074d8c05b69f1
Stewart MacKenzie-Leigh <me@stewartml.co.uk>
2013-03-23 11:14:16 +0000

    Updates screen.sh script to redirect stderr to a single log file.

--- agent/screen.sh
+++ agent/screen.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 for i in {13..20}
 do
-    screen -d -m ssh telford-$i "/home/student/c/cxb07142/PC/4th\ Year/Project/agent/run.sh | tee /home/student/c/cxb07142/PC/4th\ Year/Project/agent/runs/telford-$i-1.txt"
-    screen -d -m ssh telford-$i "/home/student/c/cxb07142/PC/4th\ Year/Project/agent/run.sh | tee /home/student/c/cxb07142/PC/4th\ Year/Project/agent/runs/telford-$i-2.txt"
+    screen -d -m ssh telford-$i "/home/student/c/cxb07142/PC/4th\ Year/Project/agent/run.sh 2> >(tee -a /home/student/c/cxb07142/PC/4th\ Year/Project/agent/error.log >&2) "
 done
 
