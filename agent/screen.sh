4074ff291d117b84bcbdd80ae3d4a4bdb5179063
Stewart MacKenzie-Leigh <me@stewartml.co.uk>
2013-03-23 11:29:50 +0000

    Updates screen.sh script to use runwithlog.sh.

--- agent/screen.sh
+++ agent/screen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {14..25}
 do
-    screen -d -m ssh telford-$i "/home/student/c/cxb07142/PC/4th\ Year/Project/agent/run.sh 2> >(tee -a /home/student/c/cxb07142/PC/4th\ Year/Project/agent/error.log >&2) "
+    screen -d -m ssh telford-$i "/home/student/c/cxb07142/PC/4th\ Year/Project/agent/runwithlog.sh"
 done
 
