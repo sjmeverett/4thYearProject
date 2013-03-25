57bd26862ce68957802c44a4df2bd3af461c9fcf
Stewart MacKenzie-Leigh <stewartml@zoho.com>
2013-03-25 17:43:47 +0000

    Adds a bunch of experiment scripts.

---
+++ agent/scripts/learning-roulette-0-pretrained-randomghosts.js
@@ -0,0 +1,13 @@
+
+/*
+Learning ghost model against the RandomGhosts controller, using 5 iterations of learning and pretrained weights.
+*/
+
+nodeExpansionThreshold = 30;
+maximumSimulationLength = 100000;
+pacManModel = new RandomNonRevPacMan();
+ghostModel = new NeuralNetworkGhostController(new RouletteMoveSelectionStrategy(), 0, true);
+tasks = [ ghostModel ];
+selectionPolicy = new LevineUcbSelectionPolicy(4000);
+opponent = new RandomGhosts();
+
